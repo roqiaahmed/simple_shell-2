b31fba97da855efa00e9a79da1ea8bdc67b3a548
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-05-24 16:20:37 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,18 +8,17 @@
  */
 int main(void)
 {
-    char *line = NULL;
+    char *line = NULL, *token;
     size_t line_size = 0;
     ssize_t line_len = 0;
     char *args[BUFFER_SIZE];
-    int status = 0;
+    int status = 0, i;
     pid_t pid;
-
+    extern char **environ;
     while (1)
     {
         printf("$ ");
         line_len = getline(&line, &line_size, stdin);
-
         if (line_len == -1)
         {
             if (feof(stdin))
@@ -33,55 +32,71 @@ int main(void)
                 exit(EXIT_FAILURE);
             }
         }
-
-        char *token = strtok(line, " \t\n");
-        int i = 0;
-
+        token = strtok(line, " \t\n");
+        i = 0;
         while (token != NULL)
         {
             args[i] = token;
             i++;
             token = strtok(NULL, " \t\n");
         }
-
         args[i] = NULL;
-
         if (args[0] == NULL)
             continue;
-
         if (strcmp(args[0], "exit") == 0)
         {
             if (args[1] != NULL)
-               status = atoi(args[1]);
-            free(line);
+                status = atoi(args[1]);
             exit(status);
         }
-
-        pid = fork();
-
-        if (pid == -1)
+        else if (strcmp(args[0], "env") == 0)
         {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pid == 0)
-        {
-            if (execvp(args[0], args) == -1)
+            char **env = environ;
+            while (*env)
             {
-                perror("execvp");
-                exit(EXIT_FAILURE);
+                printf("%s\n", *env);
+                env++;
             }
+            status = 0;
+        }
+        else if (strcmp(args[0], "setenv") == 0)
+        {
+            if (args[1] == NULL || args[2] == NULL)
+                printf("Usage: setenv <variable> <value>\n");
+            else
+                setenv(args[1], args[2], 1);
+            status = 0;
+        }
+        else if (strcmp(args[0], "unsetenv") == 0)
+        {
+            if (args[1] == NULL)
+                printf("Usage: unsetenv <variable>\n");
+            else
+                unsetenv(args[1]);
+            status = 0;
         }
         else
         {
-            wait(&status);
+            pid = fork();
+            if (pid < 0)
+            {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            else if (pid == 0)
+            {
+                if (execvp(args[0], args) == -1)
+                {
+                    perror(args[0]);
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                wait(&status);
+            }
         }
-
-        free(line);
-        line = NULL;
-        line_size = 0;
     }
-
-    return 0;
+    free(line);
+    return (0);
 }
