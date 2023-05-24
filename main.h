c1cdcc546cbc05f7ee5ead947393b2631bf45209
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-05-24 14:53:25 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define BUFFER_SIZE 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#endif /* MAIN_H */
